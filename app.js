dd8b911b3cf57f343622299be0aff458781d2bd5
Iuri de Araujo <iuri.sampaio@aln.senaicimatec.edu.br>
2023-12-26 21:43:36 -0300

    refactored and remodeled entire app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/',express.static(path.join(__dirname, 'public')));
 
 // Set up EJS as the view engine
 app.set('view engine', 'ejs');
@@ -26,28 +26,55 @@ app.get('/settings', (req, res) => {
 
 
 
-app.post('/game', (req, res) => {
-  const percentage = req.body.percentage;
-  const rounds = req.body.rounds;
-  // It seems wrong to define the array of answers previously to the game. 
-  // It sounds more reasonable/rational to calculate the array on every turn. 
-  // As an action triggered by the user 
-  res.render('game', {percentage, rounds});
+app.all('/game', (req, res) => {
+  if (req.method === 'POST') {
+    const percentage = req.body.percentage;
+    const rounds1 = req.body.rounds1;
+    const rounds2 = req.body.rounds2;
+    const limit = req.body.limit;
+
+    // It seems wrong to define the array of answers previously to the game. 
+    // It sounds more reasonable/rational to calculate the array on every turn. 
+    // As an action triggered by the user 
+    res.render('game', {percentage, rounds1, rounds2, limit});
+  } else {
+    const percentage = req.query.percentage;
+    const rounds1 = req.query.rounds1;
+    const rounds2 = req.query.rounds2;
+    const limit = req.query.limit; 
+    res.render('game2', {percentage, rounds1, rounds2, limit});
+  }
+
 });
+
+
 
 app.post('/play', (req, res) => {
   const arr = req.body.arr;
   const percentage = req.body.percentage;
-  const rounds = req.body.rounds;
-  res.render('play', {arr, percentage, rounds});
+  const rounds1 = req.body.rounds1;
+  const rounds2 = req.body.rounds2;
+  const limit = req.body.limit;
+  res.render('play', {arr, percentage, rounds1, rounds2, limit});
 });
+
+
+app.post('/play2', (req, res) => {
+  const arr = req.body.arr;
+  const percentage = req.body.percentage;
+  const rounds1 = req.body.rounds1;
+  const rounds2 = req.body.rounds2;
+  const limit = req.body.limit;
+  res.render('play2', {arr, percentage, rounds1, rounds2, limit});
+});
+
 
 app.get('/save', (req, res) => {
   res.render('save');
 });
 
 
-const port = 3000;
+const port = 3001;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
