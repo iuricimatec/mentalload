5523ad76572a91c927b5451e2b55e01fe72fc7f7
Iuri de Araujo <iuri.sampaio@aln.senaicimatec.edu.br>
2023-12-29 21:31:12 -0300

    finished 2nd release of 2nd part

--- app.js
+++ app.js
@@ -46,10 +46,10 @@ app.all('/game', (req, res) => {
     const rounds1 = req.query.rounds1;
     const rounds2 = req.query.rounds2;
     const limit = req.query.limit; 
-    const wins = req.query.wins;
-    const lefts = req.query.lefts;
+    const wins1 = req.query.wins;
+    const lefts1 = req.query.lefts;
     const results1 = req.query.results;
-    res.render('game2', {percentage, rounds1, rounds2, limit, wins, lefts, results1});
+    res.render('game2', {percentage, rounds1, rounds2, limit, wins1, lefts1, results1});
   }
 
 });
@@ -73,10 +73,10 @@ app.post('/play2', (req, res) => {
   const rounds2 = req.body.rounds2;
   const limit = req.body.limit;
   
-  const wins = req.query.wins;
-  const lefts = req.query.lefts;
-  const results1 = req.query.results;
-  res.render('play2', {arr, percentage, rounds1, rounds2, limit, wins, lefts, results1});
+  const wins1 = req.body.wins1;
+  const lefts1 = req.body.lefts1;
+  const results1 = req.body.results1;
+  res.render('play2', {arr, percentage, rounds1, rounds2, limit, wins1, lefts1, results1});
 });
 
 
